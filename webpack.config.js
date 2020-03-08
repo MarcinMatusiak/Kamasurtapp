08b8f51884f631b5f6b90ec4cbe71eecee0e1397
MarcinMatusiak <matusiakmr@gmail.com>
2020-03-08 23:43:14 +0100

    webpack, eslint, ts, babel setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  // webpack will take the files from ./src/index
+  entry: './src/index',
+
+  // and output it into /dist as bundle.js
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+
+  // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+
+        // we use babel-loader to load our jsx and tsx files
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      }/*,
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }*/
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin(/*
+      template: "./src/index.html",
+      filename: "./index.html"
+    }*/)
+  ]
+};
