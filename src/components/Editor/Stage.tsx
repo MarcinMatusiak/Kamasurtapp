3052d7d0fe0206939861f5abb6e527ad00da21e1
MarcinMatusiak <matusiakmr@gmail.com>
2020-03-30 02:06:51 +0200

    fixed stage props

--- src/components/Editor/Stage.tsx
+++ src/components/Editor/Stage.tsx
@@ -1,18 +1,20 @@
 
-import { ReactReduxContext, Provider } from 'react-redux';
-import { Stage as KonvaStage, Layer } from 'react-konva';
 import React from 'react';
+import { Layer, Stage as KonvaStage } from 'react-konva';
+import { Provider, ReactReduxContext } from 'react-redux';
 
 
-export const Stage: React.FC = () => {
+export const Stage: React.FC = ({ children, ...rest }) => {
     return (
         <ReactReduxContext.Consumer>
             {({ store }) => (
                 <KonvaStage
                     width={window.innerWidth}
-                    height={window.innerHeight}>
+                    height={window.innerHeight}
+                    { ...rest }>
                     <Provider store={store}>
                         <Layer>
+                            {children}
                         </Layer>
                     </Provider>
                 </KonvaStage>
